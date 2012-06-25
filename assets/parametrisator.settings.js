576ba2ad97a252b356d7c35b1131245783fbfbc2
ahwayakchih <ahwayakchih@gmail.com>
2012-06-25 16:40:41 +0200

    Update for Symphony 2.3. Drops compatibility with Symphony 2.2. Parameter names are now dynamically updated on data-source edit pages.

--- assets/parametrisator.settings.js
+++ assets/parametrisator.settings.js
@@ -41,18 +41,57 @@
 		// Add XSLT and XPath fields
 		var fieldset = $('fieldset.parametrisator', form);
 
-		fieldset.append('<label>'+Symphony.Language.get('XSLT Utility')+' <select name="parametrisator[xslt]">'+params['xslt']+'</select></label>');
+		fieldset.append('<div class="frame"><label>'+Symphony.Language.get('XSLT Utility')+' <select name="parametrisator[xslt]">'+params['xslt']+'</select></label></div>');
 
 		var handle = (env[0] == 'new' ? Symphony.Language.get('Untitled') : (params['handle'] != '' ? params['handle'] : env[1])),
 		    langName = Symphony.Language.get('Name'),
 		    langXPath = Symphony.Language.get('XPath');
-		    list = $('<ol class="xpaths"></ol>').appendTo($('<div class="subsection"><p class="label">Parameters</p></div>').appendTo(fieldset));
+		    list = $('<ol class="xpaths"></ol>').appendTo($('<div class="frame"><p class="label">Parameters</p></div>').appendTo(fieldset));
 		$.each(params['xpaths'], function(index, value){
-			list.append('<li class=""><h4>$ds-'+handle+'-'+index+'</h4><div class="group"><label>'+langName+' <input name="parametrisator[xpaths][name][]" value="'+index+'"/></label><label>'+langXPath+' <input name="parametrisator[xpaths][xpath][]" value="'+value.replace(/"/g, '&quot;')+'"/></label></div></li>');
+			list.append('<li data-handle="'+index+'"><header><h4><strong>$ds-'+handle+'-'+index+'</strong></h4></header><div class="two columns"><label class="column">'+langName+' <input name="parametrisator[xpaths][name][]" class="name" type="text" value="'+index+'"/></label><label class="column">'+langXPath+' <input name="parametrisator[xpaths][xpath][]" class="xpath" type="text" value="'+value.replace(/"/g, '&quot;')+'"/></label></div></li>');
 		});
 
-		list.append('<li class="template"><h4>$ds-'+handle+'-?</h4><div class="group"><label>'+langName+' <input name="parametrisator[xpaths][name][]" value=""/></label><label>'+langXPath+' <input name="parametrisator[xpaths][xpath][]" value=""/></label></div></li>');
+		list.append('<li class="template" data-handle="?"><header><h4><strong>$ds-'+handle+'-?</strong></h4></header><div class="two columns"><label class="column">'+langName+' <input name="parametrisator[xpaths][name][]" class="name" type="text" value=""/></label><label class="column">'+langXPath+' <input name="parametrisator[xpaths][xpath][]" class="xpath" type="text" value=""/></label></div></li>');
 		list.symphonyDuplicator();
+
+		function parametrisatorRegenerateHeaders(element){
+			if (!element) {
+				$('ol.xpaths li').each(function(){
+					$('header h4 strong', this).html('$ds-'+handle+'-'+$(this).attr('data-handle'));
+				});
+			}
+			else {
+				$('header h4 strong', element).html('$ds-'+handle+'-'+element.attr('data-handle'));
+			}
+		}
+
+		function parametrisatorGetHandle(text, element){
+				// From Symphony's admin.js
+				$.ajax({
+					type: 'GET',
+					data: { 'string': text },
+					dataType: 'json',
+					async: true,
+					url: Symphony.Context.get('root') + '/symphony/ajax/handle/',
+					success: function(result) {
+						if (!element) handle = result;
+						else element.attr('data-handle', result);
+						parametrisatorRegenerateHeaders(element);
+						return false;
+					}
+				});
+				// 
+		}
+
+
+		// Regenerate headers, every time user changes data-source name or param name.
+		form.find('input[name="fields[name]"]').on('blur.admin input.admin', function(){
+			parametrisatorGetHandle($(this).val());
+		});
+		form.on('blur.admin input.admin', 'ol.xpaths input.name', function(){
+			parametrisatorGetHandle($(this).val(), $(this).closest('li'));
+		});
+
 	});
 	
 })(jQuery.noConflict());
